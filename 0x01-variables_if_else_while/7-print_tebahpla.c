8b7d57acedd00c50f348f174baa8ce04bf638323
sameernegusia <sameernegusiashrif@gmail.com>
2022-12-12 12:32:22 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+	
+	while (c >= 'a')
+	{
+	putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0);
+
+}
