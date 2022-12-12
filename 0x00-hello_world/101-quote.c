be46ed462ab98a1bb2c21236afacee9451c72726
sameernegusia <sameernegusiashrif@gmail.com>
2022-12-12 09:05:10 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
  *   main - prints a quote
  *    Return:1
   */
-
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
