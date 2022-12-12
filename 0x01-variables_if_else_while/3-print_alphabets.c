c4ee1deb4b790fc03c011c8535a7a662e52566de
sameernegusia <sameernegusiashrif@gmail.com>
2022-12-12 11:18:15 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,16 @@ int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z') {
+	while (c <= 'z') 
+	{
 		putchar(c);
 		c++;						
 	}
 	c = 'A';
-	while (c <= 'Z') {			
-		putchar(c);
-		c++;
+	while (c <= 'Z') 
+	{			
+	putchar(c);
+	c++;
 	}
 	putchar('\n');
 	return (0);
