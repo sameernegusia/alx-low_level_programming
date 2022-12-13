c46c2001325e7b28b35d4e0d7b3b06d2a9dfb80b
sameernegusia <sameernegusiashrif@gmail.com>
2022-12-13 11:03:46 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *   _abs - Valor absolute
+ *   @n: int is the variable for the program.
+ *   Return: Always 0 (Success)
+  */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		return (n * -1);
+
+	}
+
+	return (n);
+
+}
