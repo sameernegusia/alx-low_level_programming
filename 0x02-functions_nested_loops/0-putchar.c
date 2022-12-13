eb42d2a91a435238dff17478aecbb88be744c194
sameernegusia <sameernegusiashrif@gmail.com>
2022-12-13 10:27:49 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ *   main - Entry point
+ *
+ *   Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar ('\n');
+	return (0);
+}
+
